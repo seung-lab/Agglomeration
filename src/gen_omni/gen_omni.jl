d5561c2e45426e623ed00e3b07e2769e334eba62
Jonathan Zung <jonathanzung@gmail.com>
2015-11-02 11:48:19 -0500

    omnification scripts added

--- src/gen_omni/gen_omni.jl
+++ src/gen_omni/gen_omni.jl
@@ -15,31 +15,23 @@ for v in [SNEMI3DTrainVolume]
 	# dend[1]=1
 	# dend[2]=2
 
+
 	#dendValues=zeros(Float32,(1,))
 	#dendValues=Float32[1.0]
 
-	# dend=zeros(UInt32,(length(edges),2))
 
-	# dendValues = zeros(Float32,(length(edges),1))
-	
-	# for (i, edge) in enumerate(edges)
-	# 	println(edge)
-	# 	dend[i,1:2] = [ UInt32(edge[1]) , UInt32(edge[2])]
-	# 	dendValues[i] = Float32(edge[3])
-	# end
 	#println(size(machine_labels))
 	#println(size(dend))
 	#println(size(dendValues))
 	dirname="$(name(v))Omni"
 	run(`mkdir -p $(dirname)`)
+
+	run(`rm -f $(dirname)/machine_labels.h5`)
 	run(`rm -f $(dirname)/image.h5`)
-	run(`cp machine_labels.h5 $(dirname)/machine_labels.h5`)
 
-	# run(`rm -f $(dirname)/machine_labels.h5`)
-
-	# h5write("$(dirname)/machine_labels.h5","/main",machine_labels)
-	# h5write("$(dirname)/machine_labels.h5","/dend", dend)
-	# h5write("$(dirname)/machine_labels.h5","/dendValues",dendValues')
+	h5write("$(dirname)/machine_labels.h5","/main",machine_labels)
+	h5write("$(dirname)/machine_labels.h5","/dend",dend)
+	#h5write("machine_labels.h5","/dendValues",dendValues)
 	h5write("$(dirname)/image.h5","/main",image)
 	run(`cat omnify.cmd`)
 	run(`echo "create:$(name(v)).omni"` |> `cat - omnify.cmd` |> "$(dirname)/omnify$(name(v)).cmd")
