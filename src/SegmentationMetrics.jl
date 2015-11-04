f3e2d9b975cbb57ae5fe7eacd6ab503fc97d405a
Jonathan Zung <jonathanzung@gmail.com>
2015-11-04 01:11:21 -0500

    modify rand error to split zero region

--- src/SegmentationMetrics.jl
+++ src/SegmentationMetrics.jl
@@ -23,13 +23,17 @@ function rand_index(A::Array,B::Array)
 			incidence_matrix[A[i],B[i]]+=1
 		end
 	end
+	reduced_incidence_matrix=incidence_matrix[:,2:end]
+	nsingletons=sum(incidence_matrix[:,1])
 
-	both=sum(incidence_matrix.^2)
+	both=sum(reduced_incidence_matrix.^2)+nsingletons
 	total_A=sum(sum(incidence_matrix,2).^2)
-	total_B=sum(sum(incidence_matrix,1).^2)
+	total_B=sum(sum(reduced_incidence_matrix,1).^2)+nsingletons
 	(:recall=> both/total_A,:precision=>both/total_B)
 end
+#=
 x=rand(1:100,(30,30))
 y=rand(1:100,(30,30))
 println(nick_index(x,y,merge_err=true,split_err=true))
+=#
 end
