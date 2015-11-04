168edd62502edfc931f31ed043717c6fd5cf8e98
Jonathan Zung <jonathanzung@gmail.com>
2015-11-04 01:13:19 -0500

    bug fix in agglomeration code

--- src/test.jl
+++ src/test.jl
@@ -9,18 +9,20 @@ using Vis
 decision_ag=DecisionTreeAgglomerator(
 Function[x->max_affinity(x[3]),
 x->measure(x[1])+measure(x[2]),
-x->measure(x[1]),
-x->measure(x[2]),
+x->min(measure(x[1]),measure(x[2])),
+x->max(measure(x[1]),measure(x[2])),
 x->contact_area(x[3]),
 x->mean_affinity(x[3])
 ]
 )
 
+#=
 linear_ag=LinearAgglomerator(
 Function[x->max_affinity(x[3])
 ],
 [1.0]
 )
+=#
 
 #Define an oracle agglomerator which will also accumulate
 #a list of all examples it has seen
@@ -42,14 +44,15 @@ print_error(rg)
 
 println("$(length(oracle.examples)) training examples")
 
+#include("curriculum.jl")
 #train the decision tree agglomerator on the set of examples
-train!(decision_ag,oracle.examples)
+experimental_train!(decision_ag,oracle.examples,OracleAgglomerator())
 
 #Run the decision tree agglomerator on a new volume.
 ag=decision_ag
 rg=atomic_region_graph(SNEMI3DTestVolume)
 print_error(rg)
-for threshold in reverse(0.3:0.05:0.8)
-	apply_agglomeration!(rg,ag,0.75)
+for threshold in reverse(0.0:0.05:0.7)
+	apply_agglomeration!(rg,ag,threshold)
 	print_error(rg)
 end
