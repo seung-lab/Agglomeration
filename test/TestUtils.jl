2c9498b5f803d92aaf28599b54499ff38f5f7607
Ignacio Tartavull <tartavull@princeton.edu>
2015-11-17 17:38:31 -0500

    copy project and change mst in tests

--- test/TestUtils.jl
+++ test/TestUtils.jl
@@ -20,12 +20,24 @@ function run_test(ag,name)
 			break
 		end
 	end
-	#=
+	
+  from = "$(dirname(@__FILE__))/../deps/datasets/ds_test/omni/Empty.omni.files"
+  to = abspath("$(dirname(@__FILE__))/../deps/datasets/ds_test/omni/$(name).omni.files")
+	
+	println("Created new project in $to")
+	cp(from,to; remove_destination=true)
+	
+  from = "$(dirname(@__FILE__))/../deps/datasets/ds_test/omni/Empty.omni"
+  to = "$(dirname(@__FILE__))/../deps/datasets/ds_test/omni/$(name).omni"
+	cp(from,to; remove_destination=true)
+	
 	mst=MST.build_mst(rg)
-	MST.saveBinary(mst)
-	=#
+	mst_path = "$(dirname(@__FILE__))/../deps/datasets/ds_test/omni/$(name).omni.files/users/_default/segmentations/segmentation1/segments/mst.data"
+	MST.saveBinary(mst,mst_path)
+
 	save_error(sm,name)
 end
+
 results_file="$(dirname(@__FILE__))/results.jls"
 function save_error(sm,name)
 	d=DataFrame(both=map(x->x[1],sm), split=map(x->x[2], sm), merge=map(x->x[3],sm), label=name)
